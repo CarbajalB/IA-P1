d5e8e999ef1076d4bc8fa6e60919210e8f0f1670
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-26 15:03:36 -0600

    Practica 1

--- _01_Variables.py
+++ _01_Variables.py
@@ -1,2 +1,6 @@
 nombre="Primer programa en Python"
+num1 = 10
+num2 = 20
+suma = num1 + num2
 print(nombre)
+print(suma)
---
+++ _04_Upper_Lower_Title.py
@@ -0,0 +1,7 @@
+cad1 = "enrique barros fernández".title()
+cad2 = "Esta Es Una Frase Para Ser Formateada."
+
+
+print(cad1)
+print(cad2.upper())
+print(cad2.lower())
