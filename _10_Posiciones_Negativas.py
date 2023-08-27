1fb6c414bf80c01d710df22b0b0144c9ff46ae10
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-26 20:59:18 -0600

    Practica 10

---
+++ _10_Posiciones_Negativas.py
@@ -0,0 +1,3 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+print("",colores[-1],"\n",colores[-7],"\n",colores[-5],"\n",colores[-2],"\n",colores[-10])
