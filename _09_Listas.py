7f5bfc4d56d1a06f445c9e94b7b8f7c9df8c3c6c
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-26 20:59:01 -0600

    Practica 9

---
+++ _09_Listas.py
@@ -0,0 +1,9 @@
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+
+print(colores[3])
+
+#El rojo esta en la posicion 0
+#eL rosa esta en la posicion 7
+
+num = ["tres","dos","cinco","cuatro","uno"]
+print(num)
