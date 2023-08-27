303e616e90f7bb743e061a1521c04e0aea09b881
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-26 20:58:20 -0600

    Practica 6

---
+++ _06_Operaciones_Basicas.py
@@ -0,0 +1,8 @@
+
+
+suma = 20 + 23 + 40
+resta = - 20 - 23 - 40
+multi = 10 * 87 
+div = 5000 / 230 /2
+oper= 10 / 5 + 15 - 17
+print("Operaciones Basicas:\nSuma = ",suma,"\nResta = ",resta,"\nMultiplicacion = ",multi,"\nDivision = ",div,"\nOperacion = ",oper)
