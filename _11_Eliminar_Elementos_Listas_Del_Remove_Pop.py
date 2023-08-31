d5e7fb2a219d903d77521df81deefe8810a39bc4
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-30 18:08:06 -0600

    Practica 11_12_13

---
+++ _11_Eliminar_Elementos_Listas_Del_Remove_Pop.py
@@ -0,0 +1,25 @@
+#Practica 11 del
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+del colores[-3]
+del colores[6]
+del colores[4]
+del colores[1]
+
+print("Practica 11: ",colores)
+
+#Practica 12 remove
+colores2 = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores2.remove("amarillo")
+colores2.remove("rojo")
+
+print("Practica 12: ",colores2)
+
+#Practica 13 pop
+colores3 = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores3.pop(1)
+colores3.pop(-2)
+
+print("Practica 13: ",colores3)
