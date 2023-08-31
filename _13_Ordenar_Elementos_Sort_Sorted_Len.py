12add0a92d6e4ba0c6d1ff5ba02b5817cc4cfefd
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-30 18:08:38 -0600

    Practica 16_17

---
+++ _13_Ordenar_Elementos_Sort_Sorted_Len.py
@@ -0,0 +1,9 @@
+#Practica 16 sort
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón','lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores.sort(reverse=True)
+print(colores)
+
+#Practica 17 len
+totalcolores = len(colores)
+print("El total de colores es de: ",totalcolores)
