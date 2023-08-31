b615110bf5c68e983315e4cb05c364e1b32c45a5
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-30 18:08:53 -0600

    Practica 18_19

---
+++ _14_Tuplas.py
@@ -0,0 +1,15 @@
+#Practica 18 Crear y Manejar Tuplas
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+
+print(colores[1])
+
+numeros = (10, 1, 5, 11)
+operacion = numeros[0] + numeros[0] + numeros[2]
+print(operacion)
+
+#Practica 19  Cómo convertir tuplas a listas y viceversa
+colores1 = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+tuplacol=tuple(colores1)
+print(type(tuplacol))
+
