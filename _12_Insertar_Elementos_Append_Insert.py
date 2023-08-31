cf42697dbbc71cbb7e3489da261ed2b6f35a765f
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-08-30 18:08:23 -0600

    Practica 14_15

---
+++ _12_Insertar_Elementos_Append_Insert.py
@@ -0,0 +1,16 @@
+#Practica 14 append
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores.append("fuxia")
+colores.append("celeste")
+
+print(colores)
+
+#Practica 15 insert
+colores1 = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores1.insert(6,"magneta")
+colores1.insert(-1,"turquesa")
+
+print(colores1)
